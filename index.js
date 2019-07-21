96ef8653d46ef561c35015ebf682598852aa5107
Thomas Rayden <thomasmwrayden@gmail.com>
2019-07-21 09:58:42 +0800

    Browser compatibility

--- index.js
+++ index.js
@@ -1,98 +1,110 @@
-/**
- * Lives
- *
- * Returns true if value exists or false if value is missing.
- *
- * @param {function} attempt
- * @returns {boolean}
- */
-function lives(attempt) {
-  let value;
-  try {
-    value = attempt();
-  } catch (err) {
-    value = undefined;
-  } finally {
-    return value !== undefined;
+(function(isNode) {
+  'use strict';
+  /**
+   * Lives
+   *
+   * Returns true if value exists or false if value is missing.
+   *
+   * @param {function} attempt
+   * @returns {boolean}
+   */
+  function lives(attempt) {
+    let value;
+    try {
+      value = attempt();
+    } catch (err) {
+      value = undefined;
+    } finally {
+      return value !== undefined;
+    }
   }
-}
 
-/**
- * Lives.not
- *
- * Returns true if value is missing or false if value exists.
- *
- * @param {function} attempt
- * @returns {boolean}
- */
-lives.not = function(attempt) {
-  let value;
-  try {
-    value = attempt();
-  } catch (err) {
-    value = undefined;
-  } finally {
-    return value === undefined;
+  /**
+   * Lives.not
+   *
+   * Returns true if value is missing or false if value exists.
+   *
+   * @param {function} attempt
+   * @returns {boolean}
+   */
+  lives.not = function(attempt) {
+    let value;
+    try {
+      value = attempt();
+    } catch (err) {
+      value = undefined;
+    } finally {
+      return value === undefined;
+    }
+  };
+
+  /**
+   * Lives.get
+   *
+   * Returns value or undefined if value is missing.
+   *
+   * @param {function} attempt
+   * @returns {any}
+   */
+  lives.get = function(attempt) {
+    let value;
+    try {
+      value = attempt();
+    } catch (err) {
+      value = undefined;
+    } finally {
+      return value;
+    }
+  };
+
+  /**
+   * Lives.or
+   *
+   * Returns value or fallback if value is missing.
+   *
+   * @param {function} attempt
+   * @param {any} fallback
+   * @returns {any}
+   */
+  lives.or = function(attempt, fallback) {
+    let value;
+    try {
+      value = attempt();
+    } catch (err) {
+      value = fallback;
+    } finally {
+      return value;
+    }
+  };
+
+  /**
+   * Lives.is
+   *
+   * Returns true if value matches type specified or false if it doesn't.
+   *
+   * @param {function} attempt
+   * @param {string} type
+   * @returns {boolean}
+   */
+  lives.is = function(attempt, type) {
+    let value;
+    try {
+      value = attempt();
+    } catch (err) {
+      value = undefined;
+    } finally {
+      return typeof value === type;
+    }
+  };
+
+  if (isNode) {
+    module.exports = lives;
+  } else {
+    window['lives'] = lives;
   }
-};
-
-/**
- * Lives.get
- *
- * Returns value or undefined if value is missing.
- *
- * @param {function} attempt
- * @returns {any}
- */
-lives.get = function(attempt) {
-  let value;
-  try {
-    value = attempt();
-  } catch (err) {
-    value = undefined;
-  } finally {
-    return value;
-  }
-};
-
-/**
- * Lives.or
- *
- * Returns value or fallback if value is missing.
- *
- * @param {function} attempt
- * @param {any} fallback
- * @returns {any}
- */
-lives.or = function(attempt, fallback) {
-  let value;
-  try {
-    value = attempt();
-  } catch (err) {
-    value = fallback;
-  } finally {
-    return value;
-  }
-};
-
-/**
- * Lives.is
- *
- * Returns true if value matches type specified or false if it doesn't.
- *
- * @param {function} attempt
- * @param {string} type
- * @returns {boolean}
- */
-lives.is = function(attempt, type) {
-  let value;
-  try {
-    value = attempt();
-  } catch (err) {
-    value = undefined;
-  } finally {
-    return typeof value === type;
-  }
-};
-
-module.exports = lives;
+})(
+  typeof module === 'object' &&
+    module &&
+    typeof module.exports === 'object' &&
+    module.exports
+);
